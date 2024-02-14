57190365e5f929744c1c7c7af66b3151b3f49208
Neptune LI <hkneptune@gmail.com>
2024-02-14 17:11:08 +0800

    Updated the unit test with the use of relative paths

--- src/test/java/com/neptuneli/pdfa_conversion/PdfaConversionTest.java
+++ src/test/java/com/neptuneli/pdfa_conversion/PdfaConversionTest.java
@@ -1,22 +1,27 @@
 package com.neptuneli.pdfa_conversion;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PdfaConversionTest {
 
-  @Test
-  void testing() throws Exception {
+  @TempDir Path tempDir;
+
+  @Test void testing() throws Exception {
 
     System.out.println("[START]");
     boolean result = false;
 
     try {
 
-      PdfaConverter.convert(
-          "D:\\temp\\TestingDocument.pdf",
-          "D:\\temp\\NewTestingDocument1.pdf", 1.7f, "3", "U");
+      ClassLoader classLoader = getClass().getClassLoader();
+      PdfaConverter.convert(classLoader.getResource("test.pdf").getPath(),
+          tempDir.resolve("testResult.pdf").toFile().getPath(),
+          1.7f, "3", "U");
 
       System.out.println("PDF conversion success");
       result = true;
